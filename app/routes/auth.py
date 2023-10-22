374a75372a82d2baf33901a89980ead0faf6c54f
Argel Asidera <argelasidera@gmail.com>
2023-10-22 20:44:21 +0800

    add auth

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -2,9 +2,10 @@ import os
 import jwt
 from flask import Blueprint
 from app.dto import loginDTO
-from app.utils import post, res_success, res_bad_request
-from app.models import User
+from app.utils import post, res_success, res_bad_request, res_forbidden
+from app.models import User, user_schema_factory
 from app.extensions import bcrypt
+from app.utils.error_handling import private_route
 
 bp = Blueprint("auth", __name__)
 
@@ -24,3 +25,15 @@ def login(payload):
         return res_success(message="Login Successful!", data={"token": token})
 
     return res_bad_request(message="Email and password not match.")
+
+
+@bp.route("/my", methods=["GET"])
+@private_route(show_loggedin_user=True)
+def get_myinfo(loggedin_user):
+    user = User.query.filter_by(id=loggedin_user.id).first()
+
+    if not user:
+        return res_forbidden(message="User not found.")
+
+    schema = user_schema_factory(exclude=["password"])
+    return res_success(data=schema.dump(user))
